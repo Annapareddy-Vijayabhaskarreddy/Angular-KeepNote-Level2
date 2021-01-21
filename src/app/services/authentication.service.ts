c42905b994b07079ee48e4ce1feeea7260bb989d
vijaya Bhaskar <vijayabhaskarreddyannapareddy@gmail.com>
2021-01-21 21:57:43 +0530

    added

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -6,14 +6,20 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AuthenticationService {
 constructor(private http:HttpClient) {}
-authenticateUser(data) {
-return this.http.post(` http://localhost:3000/auth/v1/`,data)}
-setBearerToken(token) {
-return localStorage.setItem('bearerToken',token);}
-getBearerToken() {
-return localStorage.getItem('bearerToken');}
-isUserAuthenticated(token): Promise<boolean> {
-  return this.http.post('http://localhost:3000/auth/v1/isAuthenticated',{},
-{headers:new HttpHeaders().set('Authorization','Bearer ${token}')}).map((res)=> {
-  return res['isAuthenticated']}).toPromise();}}
-
+authenticateUser(data:any) {
+      return this.http.post(`http://localhost:3000/auth/v1/`,data);
+    } 
+    setBearerToken(token:string) {
+      localStorage.setItem('bearerToken',token);
+    }
+  
+    getBearerToken() {
+      return localStorage.getItem('bearerToken');
+    }
+  
+    isUserAuthenticated(token): Promise<boolean> {
+      return this.http.post(`http://localhost:3000/auth/v1/isAuthenticated`,{},{
+        headers: new HttpHeaders().set('Authorization', `Bearer ${token}`)
+      }).map((res:any) => res.isAuthenticated).toPromise();
+  }
+}
